6d090681a6e67dc5e012905b6602784fda9ab82a
Urs <urs.gompper@gmail.com>
2019-11-12 19:08:51 +0100

    added comments + deleted unnecessary files

--- blinky_selfmade.c
+++ blinky_selfmade.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <LPC23xx.H>                    /* LPC23xx definitions                */
-//#include "LCD.h"                      	/* Graphic LCD function prototypes    */
 #include <stdbool.h>
 #include <math.h>
 
@@ -13,7 +12,6 @@
 #define BIT6 0x40
 #define BIT7 0x80
 
-
 #define RESET BIT0
 #define ENABLE BIT7
 #define SLEEP BIT1
@@ -24,39 +22,34 @@
 
 #define PI 3.14159265
 
-void LED_Init(void);
-
 void Timer_Init(void);
 __irq void T0_IRQHandler (void);
-
 
 	bool toggle;
 	int TOGGLE_STEP = STEP;
 	double sinewave;
 	int x=0;
 
-
 int main(void){
 	
-	LED_Init();
 	Timer_Init();
 	
 		/* Motor GPIO */
-	PINMODE7 = 0x00000000; // pull up resistor
+	PINMODE7 = 0x00000000; 		/* pull up resistor */
 	PINSEL7 &= 0x00000000;		/* clear P3.23..26 */
 	
 	FIO4DIR0 |= MS1 + MS2 + MS3;
 	FIO4MASK0 = 0x00000000;
-	FIO4SET0 |= MS1 + MS2 + MS3;
+	FIO4SET0 |= MS1 + MS2 + MS3; 
 	
 	FIO3DIR2 |= ENABLE;
 	FIO3MASK2 	= 0x00000000;
-	FIO3SET2 &= ~ENABLE;
+	FIO3SET2 &= ~ENABLE;			/* Enable ist low Active */
 	
 	FIO3DIR3 	|= SLEEP + RESET + STEP;
 	FIO3MASK3 	= 0x00000000;
-	FIO3SET3 |= SLEEP;
-	FIO3SET3 |= RESET;
+	FIO3SET3 |= SLEEP;				/* Sleep ist low Active */
+	FIO3SET3 |= RESET;				/* Reset ist low Active */
 	
 	while(1);
 }
@@ -64,11 +57,8 @@ int main(void){
 void LED_Init(void) { 
   PINSEL10 = 0;                         /* Disable ETM interface, enable LEDs */
   FIO2DIR  = 0x000000FF;                /* P2.0..7 defined as Outputs         */
-  FIO2MASK = 0x00000000;				/* keine Ports maskiert */
+  FIO2MASK = 0x00000000;								/* keine Ports maskiert */
   }
-
-
-
 
 void Timer_Init(void){
   /* Enable and setup timer interrupt, start timer                            */
@@ -87,24 +77,19 @@ __irq void T0_IRQHandler (void) {
 	else
 		x=0;
 	
-	sinewave = 24000*sin(((double)x/100)*PI)+48000;
-	T0MR0 				= (int)sinewave;
+	sinewave = 24000*sin(((double)x/100)*PI)+48000; /* erzeugt ein sinusförmiges Signal mit einem positiven Offset*/
+	T0MR0 				= (int)sinewave;									/* Sinussignal wird in das Timer0 Register geschrieben */
 	toggle ^= true;
 	
 	if (toggle){
-		FIO3SET3 |= STEP; // Step On
+		FIO3SET3 |= STEP; /* Step On */
 	} else {
-		FIO3CLR3 = STEP;	// Step Off
+		FIO3CLR3 = STEP;	/* Step Off */
 	}
 
-	FIO2CLR = 0xFF;                       /* Turn off all LEDs                  */
-  FIO2SET = (0x01 & 0xFF);             /* Turn on requested LEDs             */
-	
 
   T0IR        = 1;                      /* Clear interrupt flag               */
   VICVectAddr = 0;                      /* Acknowledge Interrupt              */
-	
-
 }
 
 
