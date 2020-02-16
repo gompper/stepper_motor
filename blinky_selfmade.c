07c862d29940c785ff636d959b552c96554460d4
Urs <urs.gompper@gmail.com>
2020-02-16 17:37:03 +0100

    working PWM

--- blinky_selfmade.c
+++ blinky_selfmade.c
@@ -16,6 +16,7 @@
 #define ENABLE BIT7	// P3.23
 #define SLEEP BIT1	// P3.25
 #define STEP BIT2 	// P3.26	
+
 #define MS1 BIT2	// P4.2
 #define MS2 BIT1	// P4.1
 #define MS3 BIT0	// P4.0
@@ -25,6 +26,9 @@
 
 void Timer_Init(void);
 void toggle_dir(void);
+void turn_left(void);
+void turn_right(void);
+void pwm_init(void);
 __irq void T0_IRQHandler (void);
 
 	bool toggle;
@@ -74,7 +78,10 @@ int main(void){
 		
 		FIO4SET0 |= DIR; // rotate right
 		//FIO4CLR0 = DIR; // rotate left
-
+	
+	//pwm_init();
+	
+	
 	while(1);
 }
 
@@ -86,7 +93,6 @@ void LED_Init(void) {
 
 void Timer_Init(void){
   /* Enable and setup timer interrupt, start timer                            */
-	//T0MR0         = 24000;                       /* 1msec = 12000-1 at 12.0 MHz */
 	T0MR0         = 22000;                       /* 1msec = 12000-1 at 12.0 MHz */
   T0MCR         = 3;                           /* Interrupt and Reset on MR0  */
   T0TCR         = 1;                           /* Timer0 Enable               */
@@ -96,29 +102,22 @@ void Timer_Init(void){
 }
 
 __irq void T0_IRQHandler (void) {
-
 	if (x<200)
 		x++;
 	else
 		x=0;
-	
-	sinewave = 24000*sin(((double)x/100)*PI)+48000; /* erzeugt ein sinusförmiges Signal mit einem positiven Offset*/
+	//sinewave = 24000*sin(((double)x/100)*PI)+48000; /* erzeugt ein sinusförmiges Signal mit einem positiven Offset*/
 	//T0MR0 				= (int)sinewave;									/* Sinussignal wird in das Timer0 Register geschrieben */
 	toggle ^= true;
-	
 	if (toggle){
-		FIO3SET3 |= STEP; /* Step On */
+		FIO3SET3 = STEP; /* Step On */
 	} else {
 		FIO3CLR3 = STEP;	/* Step Off */
 	}
-	
-	
 	STEP_CNT++;
 	if(STEP_CNT%1000){
 		toggle_dir();
 	}
-
-
   T0IR        = 1;                      /* Clear interrupt flag               */
   VICVectAddr = 0;                      /* Acknowledge Interrupt              */
 }
@@ -131,5 +130,32 @@ void toggle_dir(){
 	else{
 		FIO4SET0 = DIR;
 	}
+}
+
+void turn_right(){
+	FIO4SET0 = DIR;
+}
+
+void turn_left(){
+	FIO4CLR0 = DIR;
+}
+
+void pwm_init(){
+	PWM1TCR = 0x02; // Reset and disable counter for PWM
+	
+	PWM1PR	= 0x1D; // Prescale value for 1usec, Pclk=30MHz
+	PWM1MR0 = 120000;
+	PWM1MR3 = 30000;
+	PWM1MCR = 0x2;
+	PWM1LER = 0x9;
+	PWM1PCR = 0x800; // PWM3 output enabled
+	PCONP = BIT1 + BIT6 ; 	// PCPWM1 Power/Clock Control Bit
+	PCLKSEL0 = 0x00001000; 	// Set PWM Clock to CCLK/1
+	PINSEL7 |= 0x00300000;	// P3.16..31 <-- P3.26 PWM Function PWM1.3 --> PWM Channel 3 --> Single Edge PWM: Set by Match 0, Reset by Match 3
+	//PINMODE = ; // Evtl. Pull-Up / Pull-Down Widerstand an den PWM Ausgang
+	// MR0 mind. 200, MR1 = 1, MR2 = 100 bei Prozessorgeschw. von 72 MHz; ca. 72 Takte ~ 1 us
+	//PWM1CTCR = 0; kp ob das gebraucht wird
+	
+	PWM1TCR = 0x9; // Counter Enable + PWM Enable
 }
 
